c669618a3869e0a1434f587c5e70bc24f6c59e44
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2021-12-21 17:50:07 -0300

    Agregado de Foto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import NavBar from "./components/NavBar"
-import ItemListContainer from "./components/ItemListContainer";
-
+import ItemListContainer from "./components/ItemListContainer"
+import MainFoto from "./components/MainFoto"
 
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
-        
+        <MainFoto/>
         <ItemListContainer titulo="Modelos de Gorras" stock={100}/>
     </div>
   );
