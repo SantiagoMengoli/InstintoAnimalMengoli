46587ed95870c7df452b2323648f47f2766e3e27
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2022-01-24 20:56:39 -0300

    Entrega ante ultima

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail'
+import { db } from "../Firebase"
+import {collection, getDocs} from "firebase/firestore"
+
+
+console.log(db)
 
 function ItemDetailContainer() {
 
@@ -9,16 +14,38 @@ function ItemDetailContainer() {
 
     const {id} = useParams()
     
-    const productDetail =[
+   /* const productDetail =[
         {id : 1 , nombre : "Ciervo" , stock : '3' , precio: "$1500" , foto: "/img/ciervo.jpg"},
         {id : 2 , nombre : "Tigre" , stock : "3" , precio: "$1500" , foto: "/img/tigre.jpg"},
         {id : 3 , nombre : "Gallo" , stock : "3" , precio: "$1500", foto: "/img/gallo.jpg"},
         {id : 4 , nombre : "Elefante" , stock : "3" , precio: "$1500" , foto: "/img/elefante.jpg"},
         {id : 5 , nombre : "Pitbull" , stock : '3' , precio: "$1500" ,foto: "/img/pitbull.jpg"},
-    ]
+    ]*/
 
     useEffect(() => {
-        const item = productDetail.filter((product) => {
+
+        const coleccionProductos = collection(db, "productos")
+        const pedido = getDocs (coleccionProductos)
+
+        pedido
+            .then((resultado) => {
+                const docs = resultado.docs
+               const docs_formateado = docs.map(doc=>{
+                    const producto = {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+                    return producto
+                })
+                setProduct(docs_formateado)
+                setLoading(false)
+            })
+            .catch ((error) => {
+                console.log(error)
+            })
+
+
+      /*  const item = productDetail.filter((product) => {
             return product.id === +id;
         }); 
 
@@ -35,7 +62,7 @@ function ItemDetailContainer() {
             setProduct(product);
             setLoading(false);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err));*/
     }, [id]);
 
     if(loading === true) {
